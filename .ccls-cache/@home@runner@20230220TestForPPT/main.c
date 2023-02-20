fc339481bb3d7e8269eb28e66d0d947285e283d7
samhot <samhot.yang@msa.hinet.net>
2023-02-20 03:22:45 +0000

    new a function for git push testing.

--- .ccls-cache/@home@runner@20230220TestForPPT/main.c
+++ .ccls-cache/@home@runner@20230220TestForPPT/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+void testFun1()
+{
+  printf ("Create new function for test Git repository !\r\n");
+}
+
 int main(void) {
-  printf("Hello World @ 20230220 \n");
+  printf("Hello World @ 20230220 ! \r\n");
+
+  testFun1();  //create new function and push to repository.
   return 0;
 }
